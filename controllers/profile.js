8ca65b94e5782af1cd88276b85b9be074bd4ba84
Jill Marcum <jill.marcum@gmail.com>
2020-03-15 16:13:28 +0000

    initial commit

---
+++ controllers/profile.js
@@ -0,0 +1,16 @@
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select('*').from('users').where({ id })
+    .then(user => {
+      if(user.length) {
+        return res.json(user[0])
+      } else {
+        return res.status(400).json('Not found')
+      }
+    })
+    .catch(err => res.status(400).json('error retrieving user'))
+}
+
+module.exports = {
+  handleProfileGet
+};
