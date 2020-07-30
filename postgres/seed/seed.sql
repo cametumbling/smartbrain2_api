d1e96e5f9fdc9576b28177a8f38778008f3f6573
Jill Marcum <jill.marcum@gmail.com>
2020-07-29 18:22:45 -0700

    add columns and data to seed database for pet and age

--- postgres/seed/seed.sql
+++ postgres/seed/seed.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-INSERT into users (name, email, entries, joined) values ('jessie', 'jessie@gmail.com', 7, '2018-01-01');
+INSERT into users (name, email, entries, joined, pet, age) values ('jessie', 'jessie@gmail.com', 7, '2018-01-01', 'mom', 61);
 INSERT into login (hash, email) values ('$2a$10$WAK21U0LWl7C//jJ.DOB2uPP1DJQh7KUDgasdyQeGzkop2Pzl8W7u', 'jessie@gmail.com');
 
 COMMIT;
